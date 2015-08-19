4be45c8de8d53e494d010c9fcf881f6f893a7539
Andy Pohl <anypohl@gmail.com>
2015-08-19 11:31:31 -0500

    Select box works.

--- app/app.R
+++ app/app.R
@@ -3,43 +3,52 @@ library(shinydashboard)
 suppressMessages(library(fslr))
 options(fsl.path="/usr/bin")
 
+## UI PART
+
 ui <- dashboardPage(
   dashboardHeader(title = "fslr + Shiny"),
   dashboardSidebar(selectInput(inputId="filename", label="File", choices=list.files("data/"))),
   dashboardBody(
-    # Boxes need to be put in a row (or column)
     fluidRow(
+         # Width in layout adds up to 12 for all the boxes
          box(imageOutput("brainimages", height=600, width=200), width=3),
          box(title = "Controls", uiOutput("ui"), width=3)
+	 # Need another box here, something like:
+         # box(plotOutput("lineplot"), width=5)
     )
   )
 )
 
-server <- function(input, output) {
-  # Load the image once.  If we make this selectable, then it'd
-  # be a reactive load
-  img <- readNIfTI("data/4d.nii.gz")
+## SERVER PART
 
+server <- function(input, output) {
+  # Load the image at startup and when the sidebar selection changes.
+  img <- reactive({ readNIfTI(paste("data", input$filename, sep="/")) })
+
+  # Any time a slider moves, a new PNG is made and deleted.  The ortho2() function
+  # is used to have better control over the brain pane layout (with mfrow)
   output$brainimages <- renderImage({
       w <- NULL
-      if (length(dim(img)) > 3)
+      if (length(dim(img())) > 3)
          w <- input$slider_w
       width <- 200
       height <- width*3
       outfile <- tempfile(fileext = ".png")
       png(filename=outfile, width=width, height=height)
-      ortho2(img, xyz=c(input$slider_x, input$slider_y, input$slider_z), w=w, mfrow=c(3,1))
+      ortho2(img(), xyz=c(input$slider_x, input$slider_y, input$slider_z), w=w, mfrow=c(3,1))
       suppressMessages(dev.off())
       return(list(src = outfile, contentType = "image/png"))
-  }, deleteFile = FALSE)
+  }, deleteFile = TRUE)
 
+  # The slider controls are dynamic, i.e. the max values depend on the image dimensions,
+  # so they are set up on the server.
   output$ui <- renderUI({
-      img_dim = dim(img)
+      img_dim = dim(img())
       max_x = img_dim[1]
       max_y = img_dim[2]
       max_z = img_dim[3]
       max_w = img_dim[4]
-      if (length(dim(img)) > 3)
+      if (length(img_dim) > 3)
            return(list(
              "slider_x" <- sliderInput(inputId="slider_x", label="x", min=1, max=max_x, value=max_x/2, step=1),
              "slider_y" <- sliderInput(inputId="slider_y", label="y", min=1, max=max_y, value=max_y/2, step=1),
@@ -52,6 +61,12 @@ server <- function(input, output) {
              "slider_z" <- sliderInput(inputId="slider_z", label="z", min=1, max=max_z, value=max_z/2, step=1)
 	     ))
   })
+
+  # Here will go the plot code.  Something like:
+  # output$lineplot <- renderPlot({
+      # etc etc etc
+  # })
 }
 
+## Required
 shinyApp(ui, server)
